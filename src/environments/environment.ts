e0f0edfab448a9d8fcd6bd29b2e52b69f0beaa51
oem@jonas.(none) <mendesjonasc@gmail.com>
2018-12-02 18:39:48 -0200

    Criado modulo para a configuracao do apollo

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,11 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  applicationUrl: 'https://restaurant-system-api.herokuapp.com/application',
+  oAuthUrl: 'https://restaurant-system-api.herokuapp.com/oauth',
+  BusinessManagerUrl: 'https://restaurant-system-api.herokuapp.com/business-manager',
+  clientId: '6cb1bb01-f64a-11e8-8257-118e59cbdba2'
 };
 
 /*

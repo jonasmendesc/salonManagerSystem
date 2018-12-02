5ba364760322b403fa2a61cdd7d2609d6ea38a1e
oem@jonas.(none) <mendesjonasc@gmail.com>
2018-12-02 19:06:47 -0200

    Criado o modulo de core composto com o modulo do apollo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Apollo } from 'apollo-angular';
+import gql from 'graphql-tag';
 
 @Component({
   selector: 'ssm-root',
@@ -6,5 +8,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'salonManagerSystem';
+  title = 'Sistema de salão';
+
+  constructor(private apollo: Apollo) { this.createClientId(); }
+
+  createClientId() {
+
+   this.apollo.use('application').mutate({
+     mutation: gql`mutation {
+      createAuthorization( name: "Website Gerenciador" ){
+        clientId
+        clientSecret
+        name
+      }
+    }`
+   }).subscribe((response) => console.log('response', response));
+  }
 }
+
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,13 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { CoreModule } from './core/core.module';
 import { AppComponent } from './app.component';
+import { Apollo } from 'apollo-angular';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
-  imports: [
-    BrowserModule
-  ],
+  imports: [ CoreModule ],
   providers: [],
   bootstrap: [AppComponent]
 })
